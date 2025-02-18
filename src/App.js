66a1b9671fd65d035a7930bbfd3ad477b1efda28
Saroj-Sahu <sahusarojkumar2020@gmail.com>
2025-02-19 00:49:16 +0530

    Create layout and navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
+import ProductsScrape from "./pages/ProductsScrape";
+import Layout from "./pages/Layout";
+import ProductPage from "./pages/ProductPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<ProductsPage />} />
-        <Route path="products" element={<ProductsPage />} />
+        <Route index element={<Layout />} />
+        <Route path="home" element={<Layout />} />
+        <Route path="products" >
+          <Route index element={<ProductsPage />} />
+          <Route path="scrape" element={<ProductsScrape />} />
+          <Route path=":id" element={<ProductPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
